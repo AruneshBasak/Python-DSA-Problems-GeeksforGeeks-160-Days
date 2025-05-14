aaf8c1eef126ab364d38cc2d1784de294773e508
Arunesh Basak <110350616+AruneshBasak@users.noreply.github.com>
2025-05-14 18:10:47 +0530

    Create Day 12 - Max Circular Subarray Sum.py

---
+++ Day 12 - Max Circular Subarray Sum.py
@@ -0,0 +1,20 @@
+def circularSubarraySum(arr):
+    def kadane(nums):
+        max_sum = curr = nums[0]
+        for num in nums[1:]:
+            curr = max(num,curr +num)
+            max_sum = max(max_sum,curr)
+        return max_sum
+    def min_kadane(nums):
+        min_sum = curr = nums[0]
+        for num in nums[1:]:
+            curr = min (num,curr +num)
+            min_sum = min (min_sum,curr)
+        return min_sum
+    total_sum = sum(arr)
+    max_kadane = kadane(arr)
+    min_kad = min_kadane(arr)
+    
+    if max_kadane<0:
+        return max_kadane
+    return max(max_kadane,total_sum-min_kad)
